f71e2c26c1f3b5e9e9b096cea25523b26f691d63
jeevanantham <jeevanantham.n@cognizant.com>
2024-01-24 23:07:41 +0800

    [Jeeva] Repository , controllers and validtations setup

--- src/modules/teacher/services/teacher.service.ts
+++ src/modules/teacher/services/teacher.service.ts
@@ -4,6 +4,10 @@ import { StudentRegister } from '../dtos/student.register.dto';
 @Injectable()
 export class TeacherService {
   public async registerStudents(register: StudentRegister) {
+    constructor(
+        @InjectRepository(UsersSearches)
+        private usersSearchesRepository: Repository<UsersSearches>
+      ) {}
     return register;
   }
 }
